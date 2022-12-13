de9cb2b7e02a85bb999d128f0c1ac56640a0c6b0
Josh K <joshkeneda@gmail.com>
2022-12-13 00:18:50 -0600

    Update documentation

--- _static/1-exponential-functions.js
+++ _static/1-exponential-functions.js
@@ -23,5 +23,5 @@ function updateBalance () {
                 </math>`;
 }
 
-document.getElementById('dayRange').addEventListener('change', () => {updateBalance(); MathJax.typeset();});
+document.getElementById('dayRange').addEventListener('change', () => {updateBalance();});
 document.onload = updateBalance();
