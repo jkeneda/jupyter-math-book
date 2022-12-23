137955e566c03df277e7e6e06e535e86768e9ed4
Josh K <joshkeneda@gmail.com>
2022-12-22 22:34:48 -0600

    Update documentation

--- _static/1-factoring.js
+++ _static/1-factoring.js
@@ -3,13 +3,9 @@
 function randomMonic () {
     let m = Math.floor(Math.random() * 20 - 10);
     let n = Math.floor(Math.random() * 20 - 10);
-    //document.getElementById('outer-test-box').innerHTML = 'Factor <span class="math notranslate nohighlight"><mjx-container class="MathJax CtxtMenu_Attached_0" jax="CHTML" style="font-size: 113.1%; position: relative;" tabindex="0" ctxtmenu_counter="17"><mjx-math class="MJX-TEX" aria-hidden="true"><mjx-msup><mjx-mi class="mjx-i"><mjx-c class="mjx-c1D465 TEX-I"></mjx-c></mjx-mi><mjx-script style="vertical-align: 0.363em;"><mjx-mn class="mjx-n" size="s"><mjx-c class="mjx-c32"></mjx-c></mjx-mn></mjx-script></mjx-msup><mjx-mo class="mjx-n" space="3"><mjx-c class="mjx-c2B"></mjx-c></mjx-mo><mjx-mn class="mjx-n" space="3"><mjx-c class="mjx-c35"></mjx-c></mjx-mn><mjx-mi class="mjx-i"><mjx-c class="mjx-c1D465 TEX-I"></mjx-c></mjx-mi><mjx-mo class="mjx-n" space="3"><mjx-c class="mjx-c2B"></mjx-c></mjx-mo><mjx-mn class="mjx-n" space="3"><mjx-c class="mjx-c36"></mjx-c></mjx-mn></mjx-math><mjx-assistive-mml unselectable="on" display="inline"><math xmlns="http://www.w3.org/1998/Math/MathML"><msup><mi>x</mi><mn>2</mn></msup><mo>+</mo><mn>' + (m + n) + '</mn><mi>x</mi><mo>+</mo><mn>' + (m*n) + '</mn></math></mjx-assistive-mml></mjx-container></span>';
     
-    document.getElementById('monic-div').innerHTML = 'Factor <math xmlns="http://www.w3.org/1998/Math/MathML"><msup><mi>x</mi><mn>2</mn></msup>
-    <mo>' + sign2pm(m + n) + '</mo><mn>' + Math.abs(m + n) + '</mn><mi>x</mi><mo>' + sign2pm(m*n) + '</mo><mn>' + Math.abs(m*n) + '</mn></math>';
+    document.getElementById('monic-div').innerHTML = 'Factor <math xmlns="http://www.w3.org/1998/Math/MathML"><msup><mi>x</mi><mn>2</mn></msup><mo>' + sign2pm(m + n) + '</mo><mn>' + Math.abs(m + n) + '</mn><mi>x</mi><mo>' + sign2pm(m*n) + '</mo><mn>' + Math.abs(m*n) + '</mn></math>';
     // the above works, but it doesn't have cleaned-up behavior with negatives and zeros
-
-    //document.getElementById('outer-test-box').innerHTML = MathJax.tex2chtml('\\sqrt{x^2+1}', {em: 12, ex: 6, display: false}).outerHTML;
 }
 
 function sign2pm (a) {
@@ -19,5 +15,5 @@ function sign2pm (a) {
         return '+';
     }
 }
-document.getElementById('randomize-button').addEventListener('click', () => {randomMonic(); MathJax.typeset([document.getElementById('monic-div')]);}); 
-//document.onload = updateBalance();
+
+document.getElementById('randomize-button').addEventListener('click', () => {randomMonic(); MathJax.typeset([document.getElementById('monic-div')]);});
