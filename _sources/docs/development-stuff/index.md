717e3039438af411f48a4508a975aeedba0284b6
Josh K <joshkeneda@gmail.com>
2022-12-22 23:25:01 -0600

    Update documentation

--- _sources/docs/development-stuff/index.md
+++ _sources/docs/development-stuff/index.md
@@ -4,6 +4,7 @@ To do:
 - fix dns issue in github pages settings
 - fix probability distribution graph
 - fix numbering for examples from sphinx-proof
+- maybe mess with theming
 
 Notes on sphinx tabs:
 - Headers aren't recognized by the ToC if they're in tabs
--- _static/1-factoring.js
+++ _static/1-factoring.js
@@ -4,15 +4,34 @@ function randomMonic () {
     let m = Math.floor(Math.random() * 20 - 10);
     let n = Math.floor(Math.random() * 20 - 10);
     
-    document.getElementById('monic-div').innerHTML = 'Factor <math xmlns="http://www.w3.org/1998/Math/MathML"><msup><mi>x</mi><mn>2</mn></msup><mo>' + sign2pm(m + n) + '</mo><mn>' + Math.abs(m + n) + '</mn><mi>x</mi><mo>' + sign2pm(m*n) + '</mo><mn>' + Math.abs(m*n) + '</mn></math>';
-    // the above works, but it doesn't have cleaned-up behavior with negatives and zeros
+    document.getElementById('monic-div').innerHTML = 'Factor <math xmlns="http://www.w3.org/1998/Math/MathML"><msup><mi>x</mi><mn>2</mn></msup>' + middleTerm(m+n) + lastTerm(m*n) + '</math>';
 }
 
 function sign2pm (a) {
     if (Math.sign(a) < 0) {
-        return '-';
+        return '<mo>-</mo>';
+    } else if (Math.sign(a) > 0) {
+        return '<mo>+</mo>';
     } else {
-        return '+';
+        return '';
+    }
+}
+
+function middleTerm (b) {
+    if (b == 0) {
+        return '';
+    } else if (Math.abs(b) !== 1) {
+        return sign2pm(b) + '<mn>' + Math.abs(b) + '</mn>' + '<mi>x</mi>';
+    } else {
+        return sign2pm(b) + '<mi>x</mi>';
+    }
+}
+
+function lastTerm (b) {
+    if (b == 0) {
+        return '';
+    } else {
+        return sign2pm(b) + '<mn>' + Math.abs(b) + '</mn>';
     }
 }
 
