85037d334f0be9a0472ae03ca4392abf6965c8b7
Josh K <joshkeneda@gmail.com>
2023-06-29 23:33:44 -0500

    Update documentation

--- _static/matrix-TeX.js
+++ _static/matrix-TeX.js
@@ -1,6 +1,6 @@
 // Takes a javascript array and outputs the TeX to typeset that array
 
-const x = v.array([[1, 2, 3], [4, 5, 6]]);
+var x = v.array([[1, 2, 3], [4, 5, 6]]);
 
 v.toArray(x);
 console.log(v.toArray(x));
