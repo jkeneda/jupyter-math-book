f22287793cdfe979b5ea8565b9f68d844b2f439f
Josh K <joshkeneda@gmail.com>
2023-06-30 13:53:54 -0500

    Update documentation

--- _static/matrix-TeX.js
+++ _static/matrix-TeX.js
@@ -57,9 +57,9 @@ function toLaTeX(matrix, split = false, matrixType = "") {
 document.addEventListener('click', (e) => {
     const target = e.target.closest('.sd-btn-info');
 
-    var x = v.array([[1, 2, 3], [4, 5, 6]]);
+    var x = v.array([1.5]);//([[1, 2, 3], [4, 5, 6]]);
 
-    v.swap(x, 0, 1);
+    //v.swap(x, 0, 1);
 
     document.getElementById('matrix-test').innerHTML = toLaTeX(v.toArray(x), split = true);
 
