2345d105196b6960642e082641309bd9fae44723
Josh K <joshkeneda@gmail.com>
2023-06-30 13:26:06 -0500

    Update documentation

--- _static/matrix-TeX.js
+++ _static/matrix-TeX.js
@@ -1,18 +1,54 @@
 // Takes a javascript array of arrays and outputs the TeX to typeset that array
-function toLaTeX(matrix, matrixType = "b") {
+// When not split, matrixType will change the parentheses/brackets around the matrix
+// Split matrices will have an augmented column and will be bracketed
+function toLaTeX(matrix, matrixType = "b", split = false) {
     var TeX = ``;
-    TeX += `\\begin{${matrixType}matrix}\n`;
-    for (var i = 0; i < matrix.length; i++) {
-        for (var j = 0; j < matrix[i].length; j++) {
-            TeX += matrix[i][j];
-            if (j + 1 < matrix[i].length) {
-                TeX += ` & `;
-            } else {
-                TeX += ` \\\\\n`;
+    if (split) {
+        var columns = matrix[0].length
+        if (columns < 2) {
+            return toLaTeX(matrix, matrixType, split = false);
+        }
+        TeX += `\\begin{split}\n \\left[\\begin{array}{`; //Ready for column split description, e.g. cc|c
+        for (var i = 1; i < columns; i++) {
+            TeX += `c`;
+        }
+        TeX += `|c}\n`
+        for (var i = 0; i < matrix.length; i++) {
+            for (var j = 0; j < matrix[i].length; j++) {
+                TeX += matrix[i][j];
+                if (j + 1 < matrix[i].length) {
+                    TeX += ` & `;
+                } else {
+                    TeX += ` \\\\\n`;
+                }
             }
         }
+        TeX += `\\end{array}\\right]\n\\end{split}`
+    } else {
+        TeX += `\\begin{${matrixType}matrix}\n`;
+        if (matrix[0][0] = undefined) { // implies we were given a row matrix
+            for (var j = 0; j < matrix.length; j++) {
+                TeX += matrix[j];
+                if (j + 1 < matrix.length) {
+                        TeX += ` & `;
+                } else {
+                    TeX += ` \\\\\n`;
+                }
+            }
+        } else {
+            for (var i = 0; i < matrix.length; i++) {
+                for (var j = 0; j < matrix[i].length; j++) {
+                    TeX += matrix[i][j];
+                    if (j + 1 < matrix[i].length) {
+                        TeX += ` & `;
+                    } else {
+                        TeX += ` \\\\\n`;
+                    }
+                }
+            }
+        }
+        TeX += `\\end{${matrixType}matrix}`;
     }
-    TeX += `\\end{${matrixType}matrix}`;
     return TeX;
 }
 
