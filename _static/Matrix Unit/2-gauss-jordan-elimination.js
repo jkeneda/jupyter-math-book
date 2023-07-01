de7f52f59e396315cbed0f4d1f55dd54e0ecf40d
Josh K <joshkeneda@gmail.com>
2023-06-30 23:23:59 -0500

    Update documentation

--- _static/Matrix Unit/2-gauss-jordan-elimination.js
+++ _static/Matrix Unit/2-gauss-jordan-elimination.js
@@ -1,12 +1,9 @@
+var x = v.array([1, 2, 3], [4, 5, 6]);
+
 // Currently a bit dumb - the top option is just listening for *any* click
 
 document.addEventListener('click', (e) => {
-    const target = e.target.closest('.sd-btn-info');
-
-    var x = v.array([[1, 2, 3], [4, 5, 6]]);
-
     v.swap(x, 0, 1);
-
     document.getElementById('matrix-test').innerHTML = toLaTeX(v.toArray(x), split = true);
     MathJax.typeset(['.live']);
 });
