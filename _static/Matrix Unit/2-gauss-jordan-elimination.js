054f5d6dc2b38ea7386c760dea69f920be31de1a
Josh K <joshkeneda@gmail.com>
2023-06-30 23:26:11 -0500

    Update documentation

--- _static/Matrix Unit/2-gauss-jordan-elimination.js
+++ _static/Matrix Unit/2-gauss-jordan-elimination.js
@@ -1,9 +1,13 @@
+// Assumes vectoriouus and matrix-TeX have been loaded
+
 var x = v.array([1, 2, 3], [4, 5, 6]);
 
 // Currently a bit dumb - the top option is just listening for *any* click
 
 document.addEventListener('click', (e) => {
+    console.log(v.toArray(x));
     v.swap(x, 0, 1);
+    console.log(v.toArray(x));
     document.getElementById('matrix-test').innerHTML = toLaTeX(v.toArray(x), split = true);
     MathJax.typeset(['.live']);
 });
