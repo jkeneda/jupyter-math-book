9381e6a750dd41c24f38b691f41233b1e88bf286
Josh K <joshkeneda@gmail.com>
2023-07-04 20:53:07 -0500

    Update documentation

--- scripts/Matrix Unit/2-gauss-jordan-elimination.js
+++ scripts/Matrix Unit/2-gauss-jordan-elimination.js
@@ -4,12 +4,10 @@ var x = v.array([[1, 2, 3], [4, 5, 6]]);
 
 document.addEventListener('click', (e) => {
     e.stopImmediatePropagation(); // Makes sure it only fires once instead of bubbling.
-    const target = e.target.closest('.sd-btn-info') || document.body;
-
-    if (target.closest('#swap-rows')) {
+    
+    if (e.target.closest('#swap-rows')) {
         x = v.swap(x, 0, 1);
         document.getElementById('matrix-test').innerHTML = v.toLaTeX(v.toArray(x), split = true);
         MathJax.typeset(['#matrix-test']);
-        console.log('Click event fired.');
     }
 });
