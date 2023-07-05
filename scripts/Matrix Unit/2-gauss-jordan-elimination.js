659abfd7adcf913e9c082f52db6a7c1e281cb592
Josh K <joshkeneda@gmail.com>
2023-07-04 21:35:31 -0500

    Update documentation

--- scripts/Matrix Unit/2-gauss-jordan-elimination.js
+++ scripts/Matrix Unit/2-gauss-jordan-elimination.js
@@ -7,8 +7,8 @@ document.addEventListener('click', (e) => {
     
     if (e.target.closest('#swap-rows')) {
         x = v.swap(x, 0, 1);
-        document.getElementById('matrix-test').secondChild.innerHTML = v.toLaTeX(v.toArray(x), split = true);
-        // The firstChild is because the Jupyter Book typesetting will wrap the math in an extra div - we need to preserve it for spacing
+        document.getElementById('matrix-test').firstChild.nextSibling.innerHTML = v.toLaTeX(v.toArray(x), split = true);
+        // The Child shenanigans are necessary because the Jupyter Book typesetting will wrap the math in newlines and an extra div - we need to preserve it for spacing
         MathJax.typeset(['#matrix-test']);
     }
 });
